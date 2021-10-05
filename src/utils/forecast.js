007f8e24e2a519c496e99d1660fa0bd0be8bcea9
shivaganesh4 <shivaganesh826@gmail.com>
2021-10-05 20:22:57 +0530

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Humidity is ${body.current.humidity}%. `
       );
     }
   });
