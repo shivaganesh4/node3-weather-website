8979d62654d17bc91ae9b5fbd41bbfc2ddb45797
shivaganesh4 <shivaganesh826@gmail.com>
2021-10-05 16:53:16 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const url = "http://localhost:4000/weather?address=";
+const url = "/weather?address=";
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -98,6 +99,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(4000, () => {
-  console.log("Server is up on port 4000.");
+app.listen(port, () => {
+  console.log("Server is up on port" + port);
 });
