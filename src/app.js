f5c4143c4f87bd193507c0e3e738262dc44f35ab
shivaganesh4 <shivaganesh826@gmail.com>
2021-10-05 19:54:06 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
